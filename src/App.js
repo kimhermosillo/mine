e24ad3dc4e3852d77b1c3cc51aeebe2c11c90382
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-10-04 22:23:10 -0600

    final master update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './mine.png';
 import './App.css';
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">['mine']</h1>
         </header>
+        <p className="App-intro">
+          kimberly hermosillo // check back for updates
+          <p>720/675/2974</p>
+          <p><a href="mine@kimhermosillo.com">mine@kimhermosillo.com</a></p>
+
+        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default App

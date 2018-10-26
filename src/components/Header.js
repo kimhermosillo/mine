cfd0a4bd02d1688ac64eb6b68e825658b8a3756a
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-10-26 11:37:04 -0500

    added a new component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react'
 import logo from '../mine.png'
-
+import App from '../App'
 
 
 export default class Header extends React.Component {
-   render() {
+   render(toggleDarkLight) {
         return (
         <header>
           <h1>['mine']</h1>
-          
+          <p>
+
+
+
+
+
+
+  )
+}
+    
+    
+    )
+
+
+{/* /*
+function toggleDarkLight() {
+        var body = document.getElementById("body");
+        var currentClass = body.className;
+        body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}\ */}
+
+          <button type="button" name="dark_light" onclick="toggleDarkLight()" title="Toggle dark/light mode">🌛</button>
+        </p>
         </header>
         )
     }
